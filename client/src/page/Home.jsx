0eda38e7c152c3670af6201fb2cff575dfe59b04
shravani <shravani.neelagal@gmail.com>
2024-03-27 01:27:21 +0530

    first commit

--- client/src/page/Home.jsx
+++ client/src/page/Home.jsx
@@ -69,7 +69,7 @@ const Home = () => {
 export default PageHOC(
   Home,
   <>
-    Welcome to Avax Gods <br /> a Web3 NFT Card Game
+    Welcome to Token Combat <br /> a Web3 NFT Card Game
   </>,
   <>
     Connect your wallet to start playing <br /> the ultimate Web3 Battle Card
